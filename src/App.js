70935de0c39e83cba30d57be017e3509e14f2280
samamoazemi <moazemisama@gmail.com>
2022-03-18 05:37:20 -0700

    Develop useFetch component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
+import useFetch from "./hook/useFetch";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    setLoading(true);
-    setUser(null);
-    setError(null);
-
-    axios
-      .get("endPont")
-      .then((res) => {
-        setLoading(false);
-        setUser(res.data);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  const { error, loading, data } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
 
   return (
     <div className="App">
       {loading && <p>loading ...</p>}
       {error && <p>{error}</p>}
-      {user && <div>user loaded !</div>}
+      {data && (
+        <div>
+          {data.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
